403896b3550a69e82bcaf65e83f58cbbfe28e8c9
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-12-07 17:31:15 -0500

    Correcciones

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -7,13 +7,53 @@
  * graphic logo is a trademark of OpenMRS Inc.
  */
 import React from 'react';
+import { observer } from 'mobx-react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world</h1>
-      </div>
-    )
-  }
-}
+export default observer(({ form }) => (
+  <form>
+    <label htmlFor={form.$('nombreMed').id}>
+      {form.$('nombreMed').label}
+    </label>
+    <input {...form.$('nombreMed').bind()} />
+    <p>{form.$('nombreMed').error}</p>
+
+     <label htmlFor={form.$('cantidad').id}>
+      {form.$('nombreMed').label}
+    </label>
+    <input {...form.$('cantidad').bind()} />
+    <p>{form.$('cantidad').error}</p>
+
+     <label htmlFor={form.$('prov').id}>
+      {form.$('prov').label}
+    </label>
+    <input {...form.$('prov').bind()} />
+    <p>{form.$('prov').error}</p>
+
+
+     <label htmlFor={form.$('lote').id}>
+      {form.$('lote').label}
+    </label>
+    <input {...form.$('lote').bind()} />
+    <p>{form.$('lote').error}</p>
+
+
+     <label htmlFor={form.$('fechaExp').id}>
+      {form.$('fechaExp').label}
+    </label>
+    <input {...form.$('fechaExp').bind()} />
+    <p>{form.$('fechaExp').error}</p>
+
+     <label htmlFor={form.$('costo').id}>
+      {form.$('costo').label}
+    </label>
+    <input {...form.$('costo').bind()} />
+    <p>{form.$('costo').error}</p>
+
+
+    <button type="submit" onClick={form.onSubmit}>Submit</button>
+    <button type="button" onClick={form.onReset}>Reset</button>
+    <button type="button" onClick={form.onClear}>Clear</button>
+
+    <p>{form.error}</p>
+  </form>
+));
