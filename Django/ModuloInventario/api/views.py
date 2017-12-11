1ca697f4c445013bf09d31fa0c44283b3c4f5352
Carlos Manosalvas <cmanosalvas95@gmail.com>
2017-12-11 15:10:47 -0500

    Creacion de Rest API Para creacion, eliminacion y modificacion Inventar

--- Django/ModuloInventario/api/views.py
+++ Django/ModuloInventario/api/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.db import transaction
+from rest_framework.decorators import api_view
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from inventario.models import Inventario
+from .serializers import InventarioSerializer
+
+class ListarInventario(APIView):
+    def get(self, request):
+        lista=Inventario.objects.all()
+        response=InventarioSerializer(lista, many=True)
+        return Response(response.data or None)
+
+class CrearMedicina(APIView):
+    @transaction.atomic
+    def post(self, request):
+        try:
+            with transaction.atomic():
+
+                medicina = Inventario()
+                medicina.nombre = request.data['nombre']
+                medicina.cantidad = request.data['cantidad']
+                medicina.unidad = request.data['unidad']
+                medicina.proveedor = request.data['proveedor']
+                medicina.lote = request.data['lote']
+                medicina.factura = request.data['factura']
+                medicina.fecha_exp = request.data['fecha']
+                medicina.costo = request.data['costo']
+                medicina.save()
+                return Response({"Mensaje":"Medicina Agregada al inventario exitosamente"})
+
+        except:
+
+            return Response({"Mensaje":"Se produjo un Error, no se ha podido almacenar la medicina"})
+
+
+class EliminarMedicina(APIView):
+    @transaction.atomic
+    def post(self,request):
+        id=request.data['id']
+        entry = get_object_or_404(Inventario, id=id)
+        try:
+            entry.delete()
+            return Response({"Mensaje":"Se ha eliminado el registro exitosamente"})
+
+        except:
+
+            return Response({"Mensaje":"Se ha producido un error"})
+
+class ModificarMedicina(APIView):
+    @transaction.atomic
+    def post(selfself, request):
+        try:
+            with transaction.atomic():
+
+                registro=Inventario.object.get(id=request.data['id'])
+                registro.nombre = request.data['nombre']
+                registro.cantidad = request.data['cantidad']
+                registro.unidad = request.data['unidad']
+                registro.proveedor = request.data['proveedor']
+                registro.lote = request.data['lote']
+                registro.factura = request.data['factura']
+                registro.fecha_exp = request.data['fecha']
+                registro.costo = request.data['costo']
+                registro.save()
+                return Response({"Mensaje": "Medicina Modificada exitosamente"})
+        except:
+            return Response({"Mensaje": "Se produjo un Error, no se ha podido modificar la medicina"})
+
+
--- Django/ModuloInventario/inventario/models.py
+++ Django/ModuloInventario/inventario/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Inventario(models.Model):
+    OPCIONES = (('Miligramos', 'mg'), ('Mililitros', 'ml'), ('Pastillas', 'un'), ('Cajas', 'cajas'))
+    nombre = models.CharField(max_length=30, blank=False, null=False)
+    cantidad = models.PositiveSmallIntegerField(blank=True, null=True)
+    unidad = models.CharField(max_length=30, choices=OPCIONES, default='Miligramos', null=False)
+    proveedor = models.CharField(max_length=200, blank=True, null=True)
+    lote = models.CharField(max_length=50, blank=False, null=False)
+    factura = models.CharField(max_length=20, blank=False, null=False)
+    fecha_exp = models.DateField(blank=False, null=False)
+    costo = models.FloatField(blank=False, null=False)
+
+    class Meta:
+        #managed = False
+        db_table = 'Inventario'
+        unique_together = (('id', 'nombre'),)
+
+class Medicinas(models.Model):
+    OPCIONES = (('Miligramos', 'mg'), ('Mililitros', 'ml'), ('Pastillas', 'un'), ('Cajas', 'cajas'))
+    nombre = models.CharField(max_length=30, blank=False, null=False)
+    unidad_medida = models.CharField(max_length=30, choices=OPCIONES, default='Miligramos', null=False)
+    linea = models.CharField(max_length=200, blank=True, null=True)
+    codigo = models.CharField(max_length=50, blank=False, null=False)
+    cantidad_min = models.PositiveSmallIntegerField(blank=True, null=True)
+    codigo_sect = models.CharField(max_length=50, blank=False, null=False)
+    aplica_iva = models.BooleanField(blank=False, null=False)
